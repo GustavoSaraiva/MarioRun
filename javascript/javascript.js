2f62a2000013012e4615d397e52c881d8624f1a2
Andy Gustavo Pedroso Saraiva <andy.saraiva@linx.com.br>
2023-05-22 16:15:46 -0300

    Animação e resposta a teclado configurados, configurado também a colisão com o cano.

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,15 +1,53 @@
-const mario = document.querySelector('.mario-walk');
+const mariojs = document.querySelector(".mario");
+const pipejs = document.querySelector(".pipe");
+const cloudjs = document.querySelector(".cloud");
 
+function nojump()
+{
 
-const jump = () => {
-    
-    mario.classList.add('jump');
-    
-    setTimeout(() => {
-        
-         mario.classList.remove('jump');
-        
-    }, 500);
+    mariojs.classList.remove('jump-class');
+
 }
 
-document.addEventListener("keydown", jump)
+
+
+
+
+
+
+const loop =  setInterval(function () {
+
+    const pipePosition = pipejs.offsetLeft;
+    const marioPosition = +window.getComputedStyle(mariojs).bottom.replace('px', '');
+    const cloudPosition = cloudjs.offsetLeft;
+
+    if (pipePosition <= 70 && pipePosition > 0 && marioPosition < 58) {
+        pipejs.style.animation = 'none';
+        pipejs.style.left = pipePosition + 'px';
+
+
+        mariojs.style.animation = 'none';
+        mariojs.style.bottom = marioPosition + 'px';
+
+        mariojs.src = './image/game-over.png';
+        mariojs.style.width = '35px';
+        mariojs.style.marginLeft = '50px';
+
+        cloudjs.style.animation = 'none';
+        cloudjs.style.left = cloudPosition + 'px';
+
+        
+
+        clearInterval(loop);
+    }
+
+});
+
+
+
+
+document.addEventListener('keydown', () =>{
+    mariojs.classList.add('jump-class');
+
+    setTimeout(nojump, 800);
+});
